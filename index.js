66d501248e08a8c794a9fd1f47dfce5f13825886
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-21 16:04:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+
 // Write your solution in this file!
+var customerName = 'bob';
+const LeastFavoriteCustomer = 'charity';
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+   
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+     bestCustomer ='maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'maybe charity';
+}
